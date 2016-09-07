8b96a4125d07b1837518a3d09cb8cdfd42ed3d98
nowayno <yangfeng666@sina.cn>
2016-09-07 13:59:39 +0800

    finger

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score
+{
+    private int max_mission;
+    private int max_kill;
+
+    public int Max_mission
+    {
+        get
+        {
+            return max_mission;
+        }
+
+        set
+        {
+            max_mission = value;
+        }
+    }
+
+    public int Max_kill
+    {
+        get
+        {
+            return max_kill;
+        }
+
+        set
+        {
+            max_kill = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util
+{
+    static Util util;
+    public Util getInstance()
+    {
+        if (util == null)
+            util = new Util();
+        return util;
+    }
+}
