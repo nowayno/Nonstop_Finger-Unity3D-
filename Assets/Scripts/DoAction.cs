117eb41b826b255eb31807d4e7785e50f3149c0c
nowayno <yangfeng666@sina.cn>
2016-09-17 14:02:00 +0800

    finger

---
+++ Assets/Scripts/DoAction.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * 作者：佯疯(crazYoung) 
+ * 起始时间：2016/9/16 11:57:34
+ * 完成时间：
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class DoAction
+{
+    static DoAction _action;
+    static public DoAction getInstance()
+    {
+        if (_action == null)
+            _action = new DoAction();
+        return _action;
+    }
+
+    public bool writeData<T>(T t)
+    {
+        bool flag = false;
+        Util.getInstance().writeXML<T>(t);
+        return flag;
+    }
+
+    public T readData<T>(T t)
+    {
+        return readData<T>(t);
+    }
+
+    public float bloodAndMission(int which, params float[] param)
+    {
+        float blood = 0;
+        IMathUtil imathutil;
+        if (which == 0)
+        {
+            imathutil = new PlayerMath();
+            blood = imathutil.bloodAndMission(param);
+        }
+        else if (which == 1)
+        {
+            imathutil = new MonsterMath();
+            blood = imathutil.bloodAndMission(param);
+        }
+        else
+        {
+            blood = -1;
+        }
+        return blood;
+    }
+    public float actAndMission(int which, params float[] param)
+    {
+        float act = 0;
+        IMathUtil imathutil;
+        if (which == 0)
+        {
+            imathutil = new PlayerMath();
+            act = imathutil.actAndMission(param);
+        }
+        else if (which == 1)
+        {
+            imathutil = new MonsterMath();
+            act = imathutil.actAndMission(param);
+        }
+        else
+        {
+            act = -1;
+        }
+        return act;
+    }
+    public float actAndBD(int which, params float[] param)
+    {
+        float act = 0;
+        IMathUtil imathutil;
+        if (which == 0)
+        {
+            imathutil = new PlayerMath();
+            act = imathutil.actAndBD(param);
+        }
+        else if (which == 1)
+        {
+            imathutil = new MonsterMath();
+            act = imathutil.actAndBD(param);
+        }
+        else
+        {
+            act = -1;
+        }
+        return act;
+    }
+    public float addBuff(int which, params float[] param)
+    {
+        float buff = 0;
+        IMathUtil imathutil;
+        BUFF _buff = new BUFF();
+        if (which == 0)
+        {
+            imathutil = new PlayerMath();
+            buff = imathutil.addBuff(_buff, param);
+        }
+        else if (which == 1)
+        {
+            imathutil = new MonsterMath();
+            buff = imathutil.addBuff(_buff, param);
+        }
+        else
+        {
+            buff = -1;
+        }
+        return buff;
+    }
+}
+
