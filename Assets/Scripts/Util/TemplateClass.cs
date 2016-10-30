984716311ffffc2bd79368e27f69cdb70b6f9648
nowayno <yangfeng666@sina.cn>
2016-10-30 16:13:11 +0800

    finger

---
+++ Assets/Scripts/Util/TemplateClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 作者：佯疯(crazYoung) 
+ * 起始时间：2016/10/30 10:24:46
+ * 完成时间：
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class TemplateClass<T>
+{
+    protected delegate void GetNumber(int number);
+    protected event GetNumber getNumber_;
+
+    protected void GetNumber_Event(int number)
+    {
+        getNumber_(number);
+    }
+}
