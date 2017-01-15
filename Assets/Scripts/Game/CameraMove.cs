108c62157c7c43dbf107e77c66a97592b1b19a06
nowayno <yangfeng666@sina.cn>
2017-01-15 15:36:51 +0800

    finger

--- Assets/Scripts/Game/CameraMove.cs
+++ Assets/Scripts/Game/CameraMove.cs
@@ -24,7 +24,8 @@ public class CameraMove : MonoBehaviour
         Vector3 targetPos = trans.position + new Vector3(0, 2.28f, -6f);
         //Lerp方法，跟踪和被跟踪物体位置，第三个参数是一个插值，简单说就是每一次都会根据两者距离进行计算，形成一种润滑的移动。
         //可以百度线性插值
-        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime);
+        //transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime);
+        transform.position = targetPos;
         Quaternion q = Quaternion.LookRotation(trans.position - transform.position);
         //transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
         //transform.position = Vector3.Lerp(trans.position, transform.position, Time.deltaTime);
